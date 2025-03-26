e7d3959491d83df9b9a1754631c2e4cc7268d0be
Noelle <noelleransom12@gmail.com>
2025-03-26 10:33:49 -0400

    Updated links page

--- demo-website/src/components/Timestamp.js
+++ demo-website/src/components/Timestamp.js
@@ -4,9 +4,12 @@ const timeStamps = [
     pic: `${process.env.PUBLIC_URL}/backgrounds/group.jpg`,
     desc: 
     `
-    TrackTheCrowd was officially established on February 12, 2025!
+    February 12, 2025
+
+    TrackTheCrowd was officially established!
 
     Pictured here from left to right is Noelle, Joshua and Lina. Our fourth member, not pictured here, is Max.
+
     Taken on our newly obtained webcam. Thanks Paul!
     `,
   },
@@ -14,7 +17,9 @@ const timeStamps = [
     pic: `${process.env.PUBLIC_URL}/backgrounds/ttcpresentation.jpg`,
     desc: 
     `
-    We met with the TTC Manager of New Technology & Innovation, Naina Dewan, on March 13, 2025.
+    March 13, 2025
+
+    This morning, we met with the TTC Manager of New Technology & Innovation, Naina Dewan.
 
     She was kindly on board with supporting our group as our sponsor, providing informal feedback on our project as we work on it for the rest of the semester.
 
@@ -26,9 +31,13 @@ const timeStamps = [
     pic: `${process.env.PUBLIC_URL}/backgrounds/bars.jpeg`,
     desc: 
     `
-      The construction team is hard at work creating a cardboard model of a TTC car.
+      March 19, 2025
+      
+      We've split off into two teams: the backend team, responsible for creating the image recognition model, and the frontend team, responsible for creating this website and the methods of communicating crowd data to customers.
+      
+      We've also been hard at work creating a cardboard model of a TTC car. We will be using this to simulate a TTC train environment for our image recognition model.
 
-      We will be using this to simulate a TTC train environment for our image recognition model.
+      Joshua is holding two metal poles, what could they be for? Stay tuned to find out!
     `,
   },  
 ];
