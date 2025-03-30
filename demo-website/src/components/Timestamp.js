5902ea32cb65cf156c42e6b07d3bcf6d92959fc3
Joshua Li <70135829+tycelm@users.noreply.github.com>
2025-03-30 11:16:13 -0400

    hid our presentation

--- demo-website/src/components/Timestamp.js
+++ demo-website/src/components/Timestamp.js
@@ -2,8 +2,7 @@ const timeStamps = [
   {
     label: "Beginnings",
     pic: `${process.env.PUBLIC_URL}/backgrounds/group.jpg`,
-    desc: 
-    `
+    desc: `
     February 12, 2025
 
     TrackTheCrowd was officially established!
@@ -13,24 +12,21 @@ const timeStamps = [
     Taken on our newly obtained webcam. Thanks Paul!
     `,
   },
-  { label: "TTC Presentation", 
+  {
+    label: "TTC Presentation",
     pic: `${process.env.PUBLIC_URL}/backgrounds/ttcpresentation.jpg`,
-    desc: 
-    `
+    desc: `
     March 13, 2025
 
     This morning, we met with the TTC Manager of New Technology & Innovation, Naina Dewan.
 
     She was kindly on board with supporting our group as our sponsor, providing informal feedback on our project as we work on it for the rest of the semester.
-
-    Check out our presentation, complete with drawn mockups of our design:
-    https://www.canva.com/design/DAGheX3qJbw/0CwghlaoyjnsDGj5C_BGcQ/edit?utm_content=DAGheX3qJbw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
     `,
   },
-  { label: "Construction", 
+  {
+    label: "Construction",
     pic: `${process.env.PUBLIC_URL}/backgrounds/bars.jpeg`,
-    desc: 
-    `
+    desc: `
       March 19, 2025
       
       We've split off into two teams: the backend team, responsible for creating the image recognition model, and the frontend team, responsible for creating this website and the methods of communicating crowd data to customers.
@@ -39,11 +35,11 @@ const timeStamps = [
 
       Joshua's holding two metal poles, what could they be for? Stay tuned to find out!
     `,
-  },  
-  { label: "Model Test 1", 
+  },
+  {
+    label: "Model Test 1",
     pic: `${process.env.PUBLIC_URL}/backgrounds/test1.png`,
-    desc: 
-    `
+    desc: `
       March 27, 2025
       
       We began testing the image recognition model on real cardboard.
@@ -54,7 +50,7 @@ const timeStamps = [
 
       We will be tweaking the model and debugging the camera issue before our next lab time.
     `,
-  },  
+  },
 ];
 
 export default timeStamps;
